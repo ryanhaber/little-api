a1fc32e374bf5813a5562971671c098418e5ba11
Ryan Haber <ryan.haber@gmail.com>
2020-07-11 20:46:34 -0400

    adds a bit to my service for s.a.g.

--- api.py
+++ api.py
@@ -1,4 +1,5 @@
 import flask
+import json
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
@@ -6,6 +7,24 @@ app.config["DEBUG"] = True
 @app.route('/', methods=['GET'])
 def home():
 	return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+
+@app.route('/fish', methods=['GET'])
+def fish():
+	py_resource = {
+		"name": {"first": "Ryan", "last": "Haber"},
+		"age": 43,
+		"rank": "space commander",
+		"serialNumber": "00123",
+		"pets": [{
+			"name": "Goldilocks",
+			"type": "fish"
+			},
+			{"name": "Sherlock",
+			"type": "foxhound"}
+		]
+	}
+	json_resource = json.dumps(py_resource)
+	return(json_resource)
 
 
 app.run()
