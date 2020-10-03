ea5604ca43df216210723c67d6f51eb6e9f2e40f
Ryan Haber <ryan.haber@gmail.com>
2020-10-03 09:10:04 -0400

    adds path with variable, jinja2 template

--- api.py
+++ api.py
@@ -1,5 +1,11 @@
 import flask
+from flask import render_template
 import json
+import pymongo
+
+# client = pymongo.MongoClient(
+#    "mongodb+srv://ryanhaber:ryanhaber@localhost:27017/test?retryWrites=true&w=majority")
+# db = client.test
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
@@ -8,23 +14,13 @@ app.config["DEBUG"] = True
 def home():
 	return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
 
-@app.route('/fish', methods=['GET'])
-def fish():
-	py_resource = {
-		"name": {"first": "Ryan", "last": "Haber"},
-		"age": 43,
-		"rank": "space commander",
-		"serialNumber": "00123",
-		"pets": [{
-			"name": "Goldilocks",
-			"type": "fish"
-			},
-			{"name": "Sherlock",
-			"type": "foxhound"}
-		]
-	}
-	json_resource = json.dumps(py_resource)
-	return(json_resource)
+@app.route('/fish/', methods=['GET'])
+@app.route('/fish/<path:fishname>', methods=['GET'])
+def show_subpath(fishname=None):
+	if not fishname:
+		return "1"
+	else:
+		return render_template('fishfact.html', name=fishname)
 
-
+	
 app.run()
